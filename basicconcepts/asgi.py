69d418a33e72d171d42d01d92406740d8f145ef1
Rafael Mello <rafaelmello0715@outlook.com>
2020-04-21 08:39:54 -0300

    Initial Commit

---
+++ basicconcepts/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for basicconcepts project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'basicconcepts.settings')
+
+application = get_asgi_application()
