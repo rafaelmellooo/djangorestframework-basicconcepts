bf3d84bc75e464be545684fda0d8f9ab1328e66c
Rafael Mello <rafaelmello0715@outlook.com>
2020-04-21 22:54:51 -0300

    Modeling business hours APP

---
+++ businesshours/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+
+class BusinessHours(models.Model):
+    class Weekday(models.IntegerChoices):
+        MONDAY = 1
+        TUESDAY = 2
+        WEDNESDAY = 3
+        THURSDAY = 4
+        FRIDAY = 5
+        SATURDAY = 6
+        SUNDAY = 7
+
+        def __str__(self):
+            return self.value
+
+    weekday = models.IntegerField(choices=Weekday.choices)
+    opening_time = models.TimeField()
+    closing_time = models.TimeField()
+
+    class Meta:
+        verbose_name_plural = 'Business Hours'
+
+    def __str__(self):
+        return '%s, %s - %s' % (self.Weekday.choices[self.weekday][1],
+                                self.opening_time,
+                                self.closing_time)
