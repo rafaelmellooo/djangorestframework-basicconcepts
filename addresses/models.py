897c9f33d309f3d8652a84103fc44f4e86049864
Rafael Mello <rafaelmello0715@outlook.com>
2020-04-22 02:08:38 -0300

    Modeling the addresses APP

---
+++ addresses/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class Address(models.Model):
+    country = models.CharField(max_length=40, null=True)
+    uf = models.CharField(max_length=2)
+    city = models.CharField(max_length=80)
+    neighborhood = models.CharField(max_length=80)
+    street = models.CharField(max_length=80)
+    number = models.PositiveIntegerField(blank=True, null=True)
+    latitude = models.FloatField(blank=True, null=True)
+    longitude = models.FloatField(blank=True, null=True)
+
+    class Meta:
+        verbose_name_plural = 'addresses'
+
+    def __str__(self):
+        return '%s, %s - %s, %s - %s, %s' % (self.country, self.uf, self.city,
+                                             self.neighborhood, self.street,
+                                             self.number)
--- touristattractions/models.py
+++ touristattractions/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+from addresses.models import Address
 from assessments.models import Assessment
 from attractions.models import Attraction
 
@@ -10,6 +11,7 @@ class TouristAttraction(models.Model):
     is_approved = models.BooleanField(blank=True, default=False)
     attractions = models.ManyToManyField(Attraction)
     assessments = models.ManyToManyField(Assessment)
+    address = models.OneToOneField(Address, blank=True, null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return self.name
